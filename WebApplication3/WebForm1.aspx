db39ca7e3020f2acbed347181d3dac12449e606a
Muhammad syafuan Nasruddin tay <ai160140@siswa.uthm.edu.my>
2018-10-23 16:33:57 +0800

    Experiment

---
+++ WebApplication3/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server" Height="25px" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+        <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="height: 29px" Text="Button" />
+&nbsp;
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        int a;
+        double sum = 1.0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(int.TryParse(TextBox1.Text, out a))
+            {
+                a = Convert.ToInt32(TextBox1.Text);
+                for(int i=1;i<=a;i++)
+                {
+                    sum = 1.0 / Factorial(i);
+                }
+                Label1.Text = sum.ToString();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
